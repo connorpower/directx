032bacefa2b7dfa8129cc866dae4394d5bfac24a
Connor Power <connor@connorpower.com>
2022-10-28 10:12:48 +1300

    Perform a one-time setup and window proc fn exchange for new windows

--- src/win32/window.rs
+++ src/win32/window.rs
@@ -5,7 +5,7 @@ use crate::{
     win32::{debug::msgs::DebugMsg, errors::*, invoke::chk},
 };
 
-use ::std::{ffi::CString, sync::Arc};
+use ::std::{cell::Cell, ffi::CString, sync::Arc};
 use ::windows::{
     core::{s, PCSTR},
     Win32::{
@@ -14,9 +14,9 @@ use ::windows::{
         UI::WindowsAndMessaging::{
             AdjustWindowRectEx, CreateWindowExA, DefWindowProcA, GetWindowLongPtrA, LoadCursorA,
             PostQuitMessage, RegisterClassExA, SetWindowLongPtrA, ShowWindow, CREATESTRUCTA,
-            CS_HREDRAW, CS_VREDRAW, CW_USEDEFAULT, GWLP_USERDATA, IDC_ARROW, SW_SHOWNORMAL,
-            WINDOW_EX_STYLE, WM_CLOSE, WM_NCCREATE, WM_NCDESTROY, WM_PAINT, WNDCLASSEXA,
-            WS_OVERLAPPEDWINDOW,
+            CS_HREDRAW, CS_VREDRAW, CW_USEDEFAULT, GWLP_USERDATA, GWLP_WNDPROC, IDC_ARROW,
+            SW_SHOWNORMAL, WINDOW_EX_STYLE, WM_CLOSE, WM_NCCREATE, WM_NCDESTROY, WM_PAINT,
+            WNDCLASSEXA, WS_OVERLAPPEDWINDOW,
         },
     },
 };
@@ -26,12 +26,11 @@ pub struct Window<P>
 where
     P: Fn(),
 {
+    hwnd: Cell<HWND>,
     dimension: Dimension2D<i32>,
     on_paint: P,
 }
 
-// TODO: remove HWND param from handle_msg
-// TOOD: use separate setup/thunk window proc
 // TODO; unregister class
 // TODO: Arc'ed repo of window classes?
 // TOOD: test multiple instances of same window class
@@ -47,6 +46,7 @@ where
     /// Construct and display a new window.
     pub fn new(dimension: Dimension2D<i32>, name: &str, on_paint: P) -> Result<Arc<Self>> {
         let this = Arc::new(Window {
+            hwnd: Cell::new(HWND::default()),
             dimension,
             on_paint,
         });
@@ -61,7 +61,7 @@ where
             WNDCLASSEXA {
                 cbSize: ::std::mem::size_of::<WNDCLASSEXA>() as u32,
                 style: CS_HREDRAW | CS_VREDRAW,
-                lpfnWndProc: Some(Self::wnd_proc_fn),
+                lpfnWndProc: Some(Self::wnd_proc_setup),
                 lpszClassName: Self::class_name(),
                 hCursor: cursor,
                 ..Default::default()
@@ -97,6 +97,7 @@ where
         };
 
         unsafe { ShowWindow(hwnd, SW_SHOWNORMAL) };
+
         Ok(this)
     }
 
@@ -106,62 +107,15 @@ where
         self.dimension
     }
 
-    /// C-function Win32 window procedure which acts as shim and delegates to
-    /// `Self::wnd_proc`.
-    extern "system" fn wnd_proc_fn(
-        hwnd: HWND,
-        umsg: u32,
-        wparam: WPARAM,
-        lparam: LPARAM,
-    ) -> LRESULT {
-        println!("{}", DebugMsg::new(umsg, wparam, lparam));
-
-        match umsg {
-            // If we've received a create event, then we populate an `Arc`'ed
-            // reference our rust window type in the user data section of the Win32
-            // window.
-            WM_NCCREATE => {
-                let create_struct = lparam.0 as *const CREATESTRUCTA;
-                let self_ = unsafe { (*create_struct).lpCreateParams } as *const Self;
-
-                chk!(last_err; SetWindowLongPtrA(hwnd, GWLP_USERDATA, self_ as _)).unwrap();
-            }
-            // Our window is being destroyed, so we must clean up our Arc'd data.
-            WM_NCDESTROY => {
-                let self_ = chk!(last_err; SetWindowLongPtrA(hwnd, GWLP_USERDATA, 0)).unwrap()
-                    as *const Self;
-
-                // Consume and drop our Arc which was held inside the Win32
-                // window.
-                let _ = unsafe { Arc::from_raw(self_) };
-            }
-            // We are neither creating, nor destroying a window, so we must find
-            // the `wnd_proc` method on our rust window and pass the message along.
-            _ => {
-                match chk!(nonzero_isize; GetWindowLongPtrA(hwnd, GWLP_USERDATA)) {
-                    // We've received a window event but we haven't yet received
-                    // the create window event and populated the user data with
-                    // a pointer to our rust window. We cannot handle this message
-                    // yet.
-                    Err(_) => (),
-                    Ok(ptr) => {
-                        let self_ = ptr.get() as *const Self;
-
-                        unsafe {
-                            // Add extra retain for the duration of following call
-                            Arc::increment_strong_count(self_);
-                            Arc::from_raw(self_).wnd_proc(hwnd, umsg, wparam, lparam);
-                        }
-                    }
-                }
-            }
-        }
-
-        // We _always_ pass our message through to the default window procedure.
-        unsafe { DefWindowProcA(hwnd, umsg, wparam, lparam) }
+    /// Get a handle to the Win32 window's handle. This is often required when
+    /// interacting with other APIs.
+    pub fn hwnd(&self) -> HWND {
+        self.hwnd.get()
     }
 
-    fn wnd_proc(&self, _hwnd: HWND, umsg: u32, _wparam: WPARAM, _lparam: LPARAM) {
+    fn handle_message(&self, umsg: u32, wparam: WPARAM, lparam: LPARAM) {
+        println!("{}", DebugMsg::new(umsg, wparam, lparam));
+
         match umsg {
             WM_PAINT => {
                 (self.on_paint)();
@@ -169,8 +123,68 @@ where
             WM_CLOSE => {
                 unsafe { PostQuitMessage(0) };
             }
+            WM_NCDESTROY => {
+                // Our window is being destroyed, so we must clean up our Arc'd
+                // handle on the Win32 side.
+                let self_ = chk!(last_err; SetWindowLongPtrA(self.hwnd(), GWLP_USERDATA, 0))
+                    .unwrap() as *const Self;
+                let _ = unsafe { Arc::from_raw(self_) };
+            }
             _ => (),
         }
+    }
+
+    /// C-function Win32 window procedure performs one-time setup of the
+    /// structures on the Win32 side to associate our Rust object with the Win32
+    /// object.
+    extern "system" fn wnd_proc_setup(
+        hwnd: HWND,
+        umsg: u32,
+        wparam: WPARAM,
+        lparam: LPARAM,
+    ) -> LRESULT {
+        // If we've received a create event, then we populate an `Arc`'ed
+        // reference our rust window type in the user data section of the Win32
+        // window.
+        if umsg == WM_NCCREATE {
+            let create_struct = lparam.0 as *const CREATESTRUCTA;
+            // SAFETY: The `CREATESRUCTA` structure is guaranteed by the Win32
+            // API to be valid if we've received an event of type `WM_NCCREATE`.
+            let self_ = unsafe { (*create_struct).lpCreateParams } as *const Self;
+
+            // SAFETY: `self` is within an Arc which we don't release until the
+            // window is destroyed. We are still creating the window here and
+            // our message loop is single threaded so no other window activity
+            // could be happening.
+            unsafe { (*self_).hwnd.set(hwnd) };
+
+            chk!(last_err; SetWindowLongPtrA(hwnd, GWLP_USERDATA, self_ as _)).unwrap();
+            chk!(last_err; SetWindowLongPtrA(hwnd, GWLP_WNDPROC, Self::wnd_proc_thunk as _))
+                .unwrap();
+        }
+
+        // We _always_ pass our message through to the default window procedure.
+        unsafe { DefWindowProcA(hwnd, umsg, wparam, lparam) }
+    }
+
+    extern "system" fn wnd_proc_thunk(
+        hwnd: HWND,
+        umsg: u32,
+        wparam: WPARAM,
+        lparam: LPARAM,
+    ) -> LRESULT {
+        if let Ok(ptr) = chk!(nonzero_isize; GetWindowLongPtrA(hwnd, GWLP_USERDATA)) {
+            let self_ = ptr.get() as *const Self;
+
+            unsafe {
+                // Add extra retain for the duration of following call
+                Arc::increment_strong_count(self_);
+                Arc::from_raw(self_).handle_message(umsg, wparam, lparam);
+            }
+        }
+
+        // We _always_ pass our message through to the default window procedure.
+        unsafe { DefWindowProcA(hwnd, umsg, wparam, lparam) }
     }
 }
 
