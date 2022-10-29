7bc3e0f6f2c77c24846089b99de26e495c020dec
Connor Power <connor@connorpower.com>
2022-10-30 03:10:54 +1300

    Rename 'directx' cargo bin to 'app'

---
+++ app/src/trace.rs
@@ -0,0 +1,16 @@
+//! Application level tracing configuration.
+
+use ::tracing_subscriber::{prelude::*, fmt, EnvFilter};
+
+/// Sets up application level tracing using an env logger configuration.
+///
+/// ### Example Usage
+///
+/// Run the program with `RUST_LOG=trace cargo run` to capture `TRACE`-level
+/// events and upwards.
+pub(crate) fn configure() {
+    ::tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
+}
