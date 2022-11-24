33358627782aa439ef60da4b793939305872d089
Connor Power <connor@connorpower.com>
2022-11-24 21:15:10 +1300

    Simplify game window lifecycle management

--- game/src/game.rs
+++ game/src/game.rs
@@ -6,38 +6,39 @@ use ::windows::Win32::UI::WindowsAndMessaging::{
     DispatchMessageW, GetMessageW, PostQuitMessage, TranslateMessage, MSG,
 };
 
-pub struct Game {}
+pub struct Game {
+    main_window: Window,
+}
 
 impl Game {
     pub fn new() -> Self {
-        Self {}
-    }
-
-    pub async fn run(&mut self) -> Result<()> {
-        let mut main_window = Option::Some(Window::new(
+        let main_window = Window::new(
             Dimension2D {
                 width: 800,
                 height: 600,
             },
             "Main Window",
             Some(FERRIS_ICON.id().into()),
-        )?);
+        )
+        .expect("Failed to create main window");
 
+        Self { main_window }
+    }
+
+    pub async fn run(&mut self) -> Result<()> {
         let mut msg = MSG::default();
         while unsafe { GetMessageW(&mut msg, None, 0, 0) }.as_bool() {
             unsafe { TranslateMessage(&msg) };
             unsafe { DispatchMessageW(&msg) };
 
-            if main_window
-                .as_mut()
-                .map(Window::clear_close_request)
-                .unwrap_or(false)
+            if self
+                .main_window
+                .clear_close_request()
             {
                 debug!("main window requested to close");
                 unsafe {
                     PostQuitMessage(0);
                 }
-                main_window.take();
             }
         }
 
